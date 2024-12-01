1ddb26580948999ce2b5a4b48a0797fbf1f75be1
Tidiane <tijdrame@gmail.com>
2024-12-01 14:20:45 -0500

    comment duration token in minutes

--- spring-sec-demo/src/main/java/com/emard/security/jwt/JwtService.java
+++ spring-sec-demo/src/main/java/com/emard/security/jwt/JwtService.java
@@ -41,7 +41,7 @@ public class JwtService {
                 .setClaims(claims)
                 .setSubject(username)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis()+1000*60*3))
+                .setExpiration(new Date(System.currentTimeMillis()+1000*60*3))//3mn
                 .signWith(getKey()).compact()
                 ;
     }
