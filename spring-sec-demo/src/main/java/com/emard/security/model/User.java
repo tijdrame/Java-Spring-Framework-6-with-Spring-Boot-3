43ed820336ddbe3dd60a0b929147cf440c895272
Tidiane <tijdrame@gmail.com>
2024-11-30 19:10:39 -0500

    pas de securité pour register

--- spring-sec-demo/src/main/java/com/emard/security/model/User.java
+++ spring-sec-demo/src/main/java/com/emard/security/model/User.java
@@ -1,8 +1,6 @@
 package com.emard.security.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
@@ -10,6 +8,7 @@ import lombok.Data;
 @Table(name = "users")
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String username;
     private String password;
