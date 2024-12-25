03176b9417716089bb0f58681b9348de6230a857
Tidiane <tijdrame@gmail.com>
2024-12-25 18:19:41 -0500

    with docker compose for 2 containers: one for app another for postgres

--- student-app/src/main/java/com/emard/StudentController.java
+++ student-app/src/main/java/com/emard/StudentController.java
@@ -3,21 +3,28 @@ package com.emard;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @Slf4j
 @RestController
 public class StudentController {
     private final StudentRepo studentRepo;
-    public StudentController(StudentRepo studentRepo){
-        this.studentRepo = studentRepo;
-    }
+    
     @GetMapping("getStudents")
     public List<Student> getStudents(){
         log.info("all = {}", studentRepo.findAll());
         return studentRepo.findAll();
+    }
+
+    @GetMapping("addStudent")
+    public Student addStudent(){
+        Student student = new Student();
+        student.setName("Emmanuel");
+        student.setAge(25);
+        studentRepo.save(student);
+        return student;
     }
 }
